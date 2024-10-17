4438c556502989841b86c1b8f9fc462ffeb3cd63
eds20000 <158028600+eds20000@users.noreply.github.com>
2024-10-17 16:09:03 +0900

    d

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,14 +1,14 @@
-import mysql from 'mysql2/promise';
-
+require('dotenv').config();
+const mysql = require('mysql2/promise');
 // // create the connection to database
 
 console.log("Creating connection pool...");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shoppingweb',
-    // password: 'password'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
 })
 
 
